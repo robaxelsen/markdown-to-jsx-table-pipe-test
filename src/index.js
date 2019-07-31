4d2009bd8167f008b0c3600c729639737d1afc21
Robert Axelsen <robert.axelsen@ebcont.com>
2019-07-31 15:48:18 +0200

    feat: add markdown-to-jsx approach for visuals

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
+import { compiler } from 'markdown-to-jsx';
 import SimpleMarkdown from 'simple-markdown';
 import React from 'react';
 import { render } from 'react-dom';
 import table from './markdown/table.md'
 
-var mdParse = SimpleMarkdown.defaultBlockParse;
-var mdOutput = SimpleMarkdown.defaultOutput;
-
-var syntaxTree = mdParse(table);
+const mdParse = SimpleMarkdown.defaultBlockParse;
+const mdOutput = SimpleMarkdown.defaultOutput;
+const syntaxTree = mdParse(table);
 
 console.log(JSON.stringify(syntaxTree, null, 4));
+
+render(compiler(table), document.body);
+
--- src/markdown/table.md
+++ src/markdown/table.md
@@ -5,3 +5,5 @@
 | `disabled` | `disabled` | The state of the button (disabled or enabled) | `boolean`                  | `false`     |
 | `size`     | `size`     | The size of the button.                       | `"large" \| "small"`       | `'large'`   |
 | `type`     | `type`     | The type variation of the button              | `"primary" \| "secondary"` | `'primary'` |
+
+Above table is rendered with markdown-to-jsx, that uses simple-markdown. See console log for syntaxTree output from simple-markdown, which afaik the table above is building the table from.
