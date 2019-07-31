a6da81f8d99dc5b609530ff3c4a374cf7bd24722
Robert Axelsen <robert.axelsen@ebcont.com>
2019-07-31 15:30:59 +0200

    dev: switch to using simple-markdown

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import { compiler } from 'markdown-to-jsx';
+import SimpleMarkdown from 'simple-markdown';
 import React from 'react';
 import { render } from 'react-dom';
 import table from './markdown/table.md'
 
-render(compiler(table), document.body);
+var mdParse = SimpleMarkdown.defaultBlockParse;
+var mdOutput = SimpleMarkdown.defaultOutput;
+
+var syntaxTree = mdParse(table);
+
+console.log(JSON.stringify(syntaxTree, null, 4));
